72e6be871983b8f921bf2b1cf758d52d6d82286b
sachin2745 <sachin.official648@gmail.com>
2024-02-25 19:46:49 +0530

    React

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>My First .js Website</h1>
+      <img src="https://wallpapers.com/images/hd/pikachu-pictures-d0c59pwwq17g5avc.jpg" alt="" />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias adipisci quaerat aut eaque quia modi.</p>
+    </div>
+  )
+}
+
+export default Home

a9967e6de294f5bf4d44f4ce7c165b7cede4fda1
sachin2745 <sachin.official648@gmail.com>
2024-03-03 10:52:53 +0530

    update

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,87 +1,80 @@
 import React from 'react'
-import classes from './signup.module.css'
+import classes from './signup.css'
+
+
 const SignUp = () => {
   return (
     <div>
-      <div className="container-fluid">
-  {" "}
-  <div className="row no-gutter">
-    {" "}
-    <div className="col-md-6 d-none d-md-flex bg-image" />{" "}
-    <div className="col-md-6 bg-light">
-      {" "}
-      <div className="login d-flex align-items-center py-5">
-        {" "}
-        <div className="container">
-          {" "}
-          <div className="row">
-            {" "}
-            <div className="col-lg-7 col-xl-6 mx-auto">
-              {" "}
-              <h3 className="display-4">LOGIN!!</h3> <br />{" "}
-              <form>
-                {" "}
-                <div className="form-group mb-3">
-                  {" "}
-                  <input
-                    id="inputEmail"
-                    type="email"
-                    placeholder="Email address"
-                    required=""
-                    autofocus=""
-                    className="form-control rounded-pill border-0 shadow-sm px-4"
-                  />{" "}
-                </div>{" "}
-                <div className="form-group mb-3">
-                  {" "}
-                  <input
-                    id="inputPassword"
-                    type="password"
-                    placeholder="Password"
-                    required=""
-                    className="form-control rounded-pill border-0 shadow-sm px-4 text-danger"
-                  />
-                  <br />{" "}
-                </div>{" "}
-                <div className="custom-control custom-checkbox mb-3">
-                  {" "}
-                  <input
-                    id="customCheck1"
-                    type="checkbox"
-                    defaultChecked=""
-                    className="custom-control-input"
-                  />{" "}
-                  <label
-                    htmlFor="customCheck1"
-                    className="custom-control-label"
-                  >
-                    Remember password
-                  </label>{" "}
-                </div>{" "}
-                <button
-                  type="submit"
-                  className="btn btn-danger btn-block text-uppercase mb-2 rounded-pill shadow-sm"
-                >
-                  Sign in
-                </button>{" "}
-                <div className="text-center d-flex justify-content-between mt-4">
-                  <p>
-                    {" "}
-                    OR &nbsp;
-                    <a href=" " className="font-italic text-muted">
-                      {" "}
-                      <u>Create Account</u>
-                    </a>
-                  </p>
-                </div>{" "}
-              </form>{" "}
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
-    </div>{" "}
-  </div>
-</div>
+      <div className="container bg-dark ">
+        <div className="row">
+          <div className="col-md-4 shadow imageRight">
+            <div>
+              <h1 className='text-light p-5 display-1' >Sign Up</h1>
+            </div>
+          </div>
+          <div className="col-md-8 shadow">
+            <div className="container p-5 ">
+              <div className="row mb-4 gy-3">
+                <h2 className='text-white fw-bold'>Create Account</h2>
+                <div className="col-md-6"><button className='btn btn-outline-light w-100 rounded-pill'>  Sign Up with Google</button></div>
+                <div className="col-md-6"><button className='btn btn-light w-100  rounded-pill'>Sign Up with Facebook</button></div>
+              </div>
+              <div className="form-floating  py-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="formId1"
+                  id="formId1"
+                  placeholder=""
+                />
+                <label htmlFor="formId1 ">Name</label>
+              </div>
+              <div className="form-floating  py-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="formId2"
+                  id="formId2"
+                  placeholder=""
+                />
+                <label htmlFor="formId2 ">Email ID or Phone No.</label>
+              </div>
+              <div className="form-floating  py-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="formId3"
+                  id="formId3"
+                  placeholder=""
+                />
+                <label htmlFor="formId3 ">Username</label>
+              </div>
+              <div className="form-floating  py-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  name="formId4"
+                  id="formId4"
+                  placeholder=""
+                />
+                <label htmlFor="formId4 ">Password</label>
+              </div>
+              <div className="form-check py-3">
+                <input className="form-check-input" type="checkbox" value="" id="" />
+                <label className="form-check-label text-white" for=""> I agree to </label>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary rounded-pill w-100"
+              >
+                Sign Up
+              </button>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
 
     </div>
 

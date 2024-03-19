6ce9c07ec9fff823b67b475aeaeefaf9e0e42cd5
sachin2745 <sachin.official648@gmail.com>
2024-03-19 19:02:55 +0530

    update

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { IconHeart, IconShare } from '@tabler/icons-react';
+import { IconHeart, IconMessage, IconShare } from '@tabler/icons-react';
 import React, { useEffect, useState } from 'react'
 
 const Feed = () => {
@@ -33,21 +33,19 @@ const Feed = () => {
                 <hr />
                 <div className='col-md-6 mx-auto'>
                     {
-                        postArray.map((post) => {
-                            return <div key={post._id} className='card shadow mb-5'>
-                                <div className="card-header">
-                                    <h4>{post.title}</h4>
-                                </div>
-                                <img className='card-img-top' src={post.image} alt="" />
-                                <div className="card-footer">
-                                    <div className="d-flex gx-4">
-                                        <button className='btn btn-outline-warning w-100 '>{post.likes} <IconHeart></IconHeart> </button>
-                                        <button className='btn btn-outline-warning w-100 '>{post.shares} <IconShare></IconShare> </button>
-                                        
-                                    </div>
+                        postArray.map((post) => <div key={post._id} className='card shadow mb-5'>
+                            <div className="card-header">
+                                <h4>{post.title}</h4>
+                            </div>
+                            <img className='card-img-top' src={post.image} alt="" />
+                            <div className="card-footer">
+                                <div className="d-flex gx-4">
+                                    <button className='btn btn-outline-warning w-100 '>{post.likes} <IconHeart></IconHeart> </button>
+                                    <button className='btn btn-outline-warning w-100 '>{post.shares} <IconShare></IconShare> </button>
+                                    <button className='btn btn-outline-warning w-100 '>{post.shares} <IconMessage /> </button>
                                 </div>
                             </div>
-                        })
+                        </div>)
                     }
                 </div>
             </div>
